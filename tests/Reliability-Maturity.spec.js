b36e409f6eb87b7c04bce582a1bfb2977e9d5dc9
An Syafarino Armawahyudi <97266098+syafarino15@users.noreply.github.com>
2024-12-19 10:12:56 +0700

    Update Reliability Maturit

--- tests/Reliability-Maturity.spec.js
+++ tests/Reliability-Maturity.spec.js
@@ -32,13 +32,6 @@ test('Reliability-Maturity-Endurance Test', async ({ page }) => {
         console.log(`Iteration ${i + 1} completed`);
     }
 
-    // // Menyimpan hasil ke file CSV
-    // const csvFilePath = path.join(__dirname, 'Reliability-Maturity-EnduranceTest.csv');
-    // const csvHeader = 'Iteration, Status, Load Time (ms)\n';
-    // const csvContent = results.map(r => `${r.iteration}, ${r.status}, ${r.duration}`).join('\n');
-    // fs.writeFileSync(csvFilePath, csvHeader + csvContent, 'utf8');
-    // console.log('CSV report written successfully');
-
     // Menyimpan hasil ke file TXT
     const txtFilePath = path.join(__dirname, 'Reliability-Maturity-EnduranceTest.txt');
     const txtContent = results.map(r => `Iteration: ${r.iteration}, Status: ${r.status}, Load Time: ${r.duration} ms`).join('\n');
@@ -51,42 +44,3 @@ test('Reliability-Maturity-Endurance Test', async ({ page }) => {
     fs.writeFileSync(jsonFilePath, jsonContent, 'utf8');
     console.log('JSON report written successfully');
 });
-
-// // Jumlah pengulangan pengujian
-// const iterations = 50;
-
-// test('Reliability-Maturity-Endurance Test', async ({ page }) => {
-//     const results = [];
-
-//     for (let i = 0; i < iterations; i++) {
-
-//         const startTime = Date.now();
-
-//         await page.goto('https://opibox.netlify.app/', { waitUntil: 'load', timeout: 10000 });
-//         await page.getByRole('link', { name: 'Masuk' }).click();
-//         await page.getByRole('textbox', { name: 'Masukkan email' }).fill('syafarino010502@gmail.com');
-//         await page.getByRole('textbox', { name: 'Masukkan kata sandi' }).fill('Syafarino_010502');
-//         await page.getByRole('button', { name: 'Masuk' }).click();
-
-//         // Verifikasi bahwa login berhasil
-//         await expect(page.getByRole('heading', { name: 'Proyek Saya' })).toBeVisible();
-
-//         const endTime = Date.now();
-//         const duration = endTime - startTime;
-//         results.push({ iteration: i, status: 'Success', duration });
-
-//         // Logout dan ulangi
-//         await page.getByRole('link', { name: 'Pengaturan' }).click();
-//         await page.click('#logout');
-//         await page.getByRole('button', { name: 'Ya' }).click();
-
-//         console.log(`Iteration ${i + 1} completed`);
-//     }
-
-//     const csvFilePath = path.join(__dirname, 'Reliability-Availability-EnduranceTest.csv');
-//     const csvHeader = 'Iteration, Status, Load Time (ms)\n';
-//     const csvContent = results.map(r => `${r.iteration}, ${r.status}, ${r.duration}`).join('\n');
-
-//     fs.writeFileSync(csvFilePath, csvHeader + csvContent, 'utf8');
-//     console.log('Report writen successfully');
-// });
